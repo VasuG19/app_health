2a3725f2e127a628a660a285ba56b47b48b709bd
Vasu <99899682+VasuG19@users.noreply.github.com>
2023-04-15 05:25:08 +0100

    updates

--- src/pages/appointment.js
+++ src/pages/appointment.js
@@ -109,7 +109,7 @@ function Appointment(props) {
             // days of week. an array of zero-based day of week integers (0=Sunday)
             daysOfWeek: [ 1, 2, 3, 4, 5 ], // Monday - Friday
 
-            startTime: '10:00', // a start time (10am in this example)
+            startTime: '09:00', // a start time (9am in this example)
             endTime: '18:00', // an end time (6pm in this example)
           }}
         />
