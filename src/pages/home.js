7e6afe59ff1033638e304b753b02b9e168897cc4
Vasu <99899682+VasuG19@users.noreply.github.com>
2023-04-17 23:52:49 +0100

    home page updates

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,50 +1,55 @@
-import {React} from 'react';
-import { Button, Card } from 'react-bootstrap';
-import { Carousel } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Button, Card, Carousel } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 
-/**
- * Home Page
- * 
- * function to return home page data with relevant information and images
- * 
- * @author Mehtab Gill
- */
+function HomePage(props) {
+  const [data, setData] = useState([]);
 
-function HomePage(props){
-const [data, setData] = useState([]);
-
-// retrieve appointments from API
-  useEffect( () => {
+  useEffect(() => {
     fetch(`http://localhost:1337/api/appointments?populate=*&filters[patient]$eq]=${props.user.id}`)
-    .then((response) => response.json())
-    .then((json) => {setData(json.data)})
-    .catch((err) => {console.log(err.message)});
-    }, [props.user.id]);
+      .then((response) => response.json())
+      .then((json) => setData(json.data))
+      .catch((err) => console.log(err.message));
+  }, [props.user.id]);
 
-    const accountData = data.map((value) =>
-        <Carousel.Item key={value.id}>
-            <Card className="text-center" border='dark'>
-                <Card.Body className='appointmentCard'>
-                    <Card.Title>{value.attributes.title}</Card.Title>
-                    <Card.Text>{value.attributes.start}</Card.Text>
-                        <Link to={`/Appointment`}>
-                            <Button type="submit"> View calendar </Button>
-                        </Link>
-                </Card.Body>
-            </Card> 
-        </Carousel.Item>
-    )
+  const chunks = data.reduce((acc, curr, i) => {
+    if (i % 3 === 0) acc.push([]);
+    acc[acc.length - 1].push(curr);
+    return acc;
+  }, []);
 
-    return(
-        <div className='home'>
-            <Carousel prevIcon={<FontAwesomeIcon icon={faChevronLeft} />} nextIcon={<FontAwesomeIcon icon={faChevronRight}/>} indicators={false} >
-                        {accountData}
-            </Carousel>
-        </div>
-    )}
+  const accountData = chunks.map((chunk, i) => (
+    <Carousel.Item key={i}>
+      <div className="d-flex justify-content-around">
+        {chunk.map((value) => (
+          <Card className="text-center" border="dark" key={value.id}>
+            <Card.Body className="appointmentCard">
+                <Card.Title>{value.attributes.title}</Card.Title>
+                <Card.Text>{value.attributes.start}</Card.Text>
+                <Link to={`/Appointment`}>
+                <Button type="submit"> View calendar </Button>
+                </Link>
+            </Card.Body>
+          </Card>        
+        ))}
+      </div>
+    </Carousel.Item>
+  ));
 
-export default HomePage
+  return (
+    <div className="home">
+      <Carousel
+        prevIcon={<FontAwesomeIcon icon={faChevronLeft} />}
+        nextIcon={<FontAwesomeIcon icon={faChevronRight} />}
+        indicators={false}
+        interval={null} // Set interval to null to display all items at once
+      >
+        {accountData}
+      </Carousel>
+    </div>
+  );
+}
+
+export default HomePage;
