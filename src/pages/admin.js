c88d0ac3eb910f629ae90c5922fe289bd174cf09
Vasu <99899682+VasuG19@users.noreply.github.com>
2023-04-21 18:18:48 +0100

    admin uodates

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -64,125 +64,71 @@ function Admin (props){
         <MDBContainer className="py-5">
             <h1><strong>Admin</strong></h1>
             <MDBRow>
-            <MDBCol lg="4">
-                <MDBCard className="mb-4 profile">
-                <MDBCardBody className="text-center">
-                    <MDBCardImage
-                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
-                    alt="avatar" className="rounded-circle" style={{ width: '150px' }} fluid />
-                    <div className='profileButton'><p className="text-muted mb-1">{props.user.username}</p></div>
-                    <div className="d-flex justify-content-center mb-2">
-                        <div className='profileButton'>
-                            <Button className="logout" type="button" value="Sign out" onClick={handleSignOut}>Sign out</Button>
+
+                <MDBCol lg="4">
+                    <MDBCard className="mb-4 profile">
+                    <MDBCardBody className="text-center">
+                        <MDBCardImage
+                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                        alt="avatar" className="rounded-circle" style={{ width: '150px' }} fluid />
+                        <div className='profileButton'><p className="text-muted mb-1">{props.user.username}</p></div>
+                        <div className="d-flex justify-content-center mb-2">
+                            <div className='profileButton'>
+                                <Button className="logout" type="button" value="Sign out" onClick={handleSignOut}>Sign out</Button>
+                            </div>
                         </div>
-                    </div>
-                </MDBCardBody>
-                </MDBCard>
-                <MDBCard className="mb-4 mb-lg-0 profile">
-                <MDBCardBody>
-                    <MDBRow>
-                    <MDBCol sm="4">
-                        <MDBCardText>Height</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="8">
-                        <MDBCardText className="text-muted">{props.user.height}</MDBCardText>
-                    </MDBCol>
-                    </MDBRow>
-                    <hr />
-                    <MDBRow>
-                    <MDBCol sm="4">
-                        <MDBCardText>Weight</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="8">
-                        <MDBCardText className="text-muted">{props.user.weight}</MDBCardText>
-                    </MDBCol>
-                    </MDBRow>
-                    <hr />
-                    <MDBRow>
-                    <MDBCol sm="4">
-                        <MDBCardText>Blood Type</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="8">
-                        <MDBCardText className="text-muted">{props.user.blood_type}</MDBCardText>
-                    </MDBCol>
-                    </MDBRow>
-                    <hr />
-                    <MDBRow>
-                    <MDBCol sm="4">
-                        <MDBCardText>Diet</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="8">
-                        <MDBCardText className="text-muted">{props.user.diet}</MDBCardText>
-                    </MDBCol>
-                    </MDBRow>
-                    <hr />
-                    <MDBRow>
-                    <MDBCol sm="4">
-                        <MDBCardText>Pregnant</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="8">
-                        <MDBCardText className="text-muted">{props.user.pregnant}</MDBCardText>
-                    </MDBCol>
-                    </MDBRow>
-                    <hr />
-                    <MDBRow>
-                    <MDBCol sm="4">
-                        <MDBCardText>Smoke</MDBCardText>
-                    </MDBCol>
-                    <MDBCol sm="8">
-                        <MDBCardText className="text-muted">{props.user.smoke}</MDBCardText>
-                    </MDBCol>
-                    </MDBRow>
-                </MDBCardBody>
-                </MDBCard>
-            </MDBCol>
+                    </MDBCardBody>
+                    </MDBCard>
 
-            <MDBCol lg="8">
-                <MDBCard className="mb-4 profile">
-                <MDBCardBody>
-                    <MDBRow>
-                    <MDBCol>
-                        <MDBCardText> List of patients </MDBCardText>
-                    </MDBCol>
-                    </MDBRow>
-                    <hr />
-                    <MDBRow style={{ height: '200px', overflow: 'scroll' }}>
-                        {allPatients}
-                    </MDBRow>
-                    <hr />
-                </MDBCardBody>
-                </MDBCard>
-
-                <MDBRow>
-                <MDBCol md="4">
-                    <MDBCard className="mb-4 mb-md-0 profile">
+                    <MDBCard className="mb-4 mb-lg-0 profile">
                     <MDBCardBody>
-                        <MDBCardText className="mb-4"><span className="text-primary font-italic me-1">Number of patients</span></MDBCardText>
-                        <MDBCardText className="text-muted">{patientNo}</MDBCardText>
+                        <MDBRow>
+                        <MDBCol sm="4">
+                            <MDBCardText>Patients</MDBCardText>
+                        </MDBCol>
+                        <MDBCol sm="8">
+                            <MDBCardText className="text-muted">{patientNo}</MDBCardText>
+                        </MDBCol>
+                        </MDBRow>
+                        <hr />
+                        <MDBRow>
+                        <MDBCol sm="5">
+                            <MDBCardText>Upcoming Appointments</MDBCardText>
+                        </MDBCol>
+                        <MDBCol sm="8">
+                            <MDBCardText className="text-muted">{props.user.weight}</MDBCardText>
+                        </MDBCol>
+                        </MDBRow>
+                        <hr />
+                        <MDBRow>
+                        <MDBCol sm="5">
+                            <MDBCardText>Total Appointments</MDBCardText>
+                        </MDBCol>
+                        <MDBCol sm="8">
+                            <MDBCardText className="text-muted">{props.user.blood_type}</MDBCardText>
+                        </MDBCol>
+                        </MDBRow>
+                    </MDBCardBody>
+                    </MDBCard>
+
+                </MDBCol>
+
+                <MDBCol lg="8">
+                    <MDBCard className="mb-4 profile">
+                    <MDBCardBody>
+                        <MDBRow>
+                        <MDBCol>
+                            <MDBCardText> List of patients </MDBCardText>
+                        </MDBCol>
+                        </MDBRow>
+                        <hr />
+                        <MDBRow style={{ height: '400px', overflow: 'scroll' }}>
+                            {allPatients}
+                        </MDBRow>
+                        <hr />
                     </MDBCardBody>
                     </MDBCard>
                 </MDBCol>
-
-                <MDBCol md="4">
-                    <MDBCard className="mb-4 mb-md-0 profile">
-                    <MDBCardBody>
-                        <MDBCardText className="mb-4"><span className="text-primary font-italic me-1">Conditions</span></MDBCardText>
-                        <MDBCardText className="text-muted">{props.user.current_conditions}</MDBCardText>
-                    </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-
-                <MDBCol md="4">
-                    <MDBCard className="mb-4 mb-md-0 profile">
-                    <MDBCardBody>
-                        <MDBCardText className="mb-4"><span className="text-primary font-italic me-1">Allergies</span></MDBCardText>
-                        <MDBCardText className="text-muted">{props.user.allergies}</MDBCardText>
-                    </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-                </MDBRow>
-
-            </MDBCol>
 
             </MDBRow>
         </MDBContainer>
