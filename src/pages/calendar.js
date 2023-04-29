ecdebeb2456a32c7c71ef7330bca065ed9764593
Vasu <99899682+VasuG19@users.noreply.github.com>
2023-04-29 18:29:54 +0100

    Update calendar.js

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -169,8 +169,8 @@ function Timetable(props) {
               startTime: '08:00', // a start time (10am in this example)
               endTime: '18:00', // an end time (6pm in this example)
               daysOfWeek: [ 1, 2, 3, 4, 5 ] }} // Monday - Friday
-            slotMinTime={'06:00'}
-            slotMaxTime={'19:00'}
+            slotMinTime={'05:00'}
+            slotMaxTime={'21:00'}
           />
         </div>
         {selectedDate && (
