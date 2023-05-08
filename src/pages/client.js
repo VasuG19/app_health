5750599bdc96edff3f908b8107e526a4b992e6c9
Vasu <99899682+VasuG19@users.noreply.github.com>
2023-05-08 23:19:42 +0100

    done

--- src/pages/client.js
+++ src/pages/client.js
@@ -1,9 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Popup from "../componants/popup";
-import { Container, Row, Card, Col } from "react-bootstrap";
-import ClientCal from "../componants/clientCal";
+import { Container, Row, Card, Col, Button, Modal } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import ClientCal from "../componants/clientCal";
 
 /**
  * Admin Page 
@@ -13,80 +12,90 @@ import { useNavigate } from "react-router-dom";
  * @author Mehtab Gill
  */
 
-function Clients (props){
-  // States used in the component
+function Clients(props) {
   const [clients, setClients] = useState([]);
+  const [show, setShow] = useState(false); // state for modal show/hide
+  const [selectedClient, setSelectedClient] = useState(null); // state for selected client
 
-  // if the user is the admin, redirect to the admin page
   const nav = useNavigate();
-  useEffect(() => {
-    if (!props.user ||props.user.title!== 'client') {
-        nav("/clients");
-      } else {
-        nav("/clientError");
-      }
-  },[nav, props.user]);
 
-    // Use Effect to fetch data on mount and when dependencies change
   useEffect(() => {
-          try {
-          const getUserData = async () => {
-              // Get patient data
-              const response = await axios.get('http://localhost:1337/api/clients?populate=*');
-              setClients(response.data.data);
-          }
-          getUserData()
-          } catch (error) {
-              console.error(error.message);
-          }        
-    },[props.user]);
+    if (!props.user || props.user.title !== "client") {
+      nav("/clients");
+    } else {
+      nav("/clientError");
+    }
+  }, [nav, props.user]);
 
-    // Map through patients and create card for each
-    const allCients = clients && clients.map((value) =>  
-            <Col  sm={true} key={value.id}>
-                <Card>
-                    <Card.Body>
-                    <Row>
-                    <Col className='clientName' sm="true">
-                        <Card.Text>{value.attributes.username}</Card.Text>
-                    </Col>
-                  </Row>
-                  <hr />
-                  <Row>
-                    <Col sm="3">
-                      <Card.Text>Institute</ Card.Text>
-                    </Col>
-                    <Col sm="9">
-                      <Card.Text className="text-muted">{value.attributes.institute}</ Card.Text>
-                    </Col>
-                  </Row>
-                  <hr/>
-                  <Row>
-                    <Col sm="3">
-                      <Card.Text>Address</ Card.Text>
-                    </Col>
-                    <Col sm="9">
-                      <Card.Text className="text-muted">{value.attributes.address}</ Card.Text>
-                    </Col>
-                  </Row>
-                  <hr/>
-                  <Row>
-                    <Col sm="3">
-                      <Card.Text>Role</ Card.Text>
-                    </Col>
-                    <Col sm="9">
-                      <Card.Text className="text-muted">{value.attributes.role}</ Card.Text>
-                    </Col>
-                  </Row>
-                  <hr/>
-                  <div style={{ height: '250px', overflow: 'scroll' }} >
-                        <Popup clients={value} />
-                  </div>
-                        <ClientCal user={props.user} client={value}/>
-                    </Card.Body>
-                </Card>
-            </Col>
-    );
+  useEffect(() => {
+    try {
+      const getUserData = async () => {
+        const response = await axios.get(
+          "http://localhost:1337/api/clients?populate=*"
+        );
+        setClients(response.data.data);
+      };
+      getUserData();
+    } catch (error) {
+      console.error(error.message);
+    }
+  }, [props.user]);
+
+  const handleClientClick = (client) => {
+    setSelectedClient(client);
+    setShow(true);
+  };
+
+  const allCients =
+    clients &&
+    clients.map((value) => (
+      <Col sm={true} key={value.id}>
+        <Card>
+          <Card.Body>
+            <Row>
+              <Col className="clientName" sm="true">
+                <Card.Text>{value.attributes.username}</Card.Text>
+              </Col>
+            </Row>
+            <hr />
+            <Row>
+              <Col sm="3">
+                <Card.Text>Institute</Card.Text>
+              </Col>
+              <Col sm="9">
+                <Card.Text className="text-muted">
+                  {value.attributes.institute}
+                </Card.Text>
+              </Col>
+            </Row>
+            <hr />
+            <Row>
+              <Col sm="3">
+                <Card.Text>Address</Card.Text>
+              </Col>
+              <Col sm="9">
+                <Card.Text className="text-muted">
+                  {value.attributes.address}
+                </Card.Text>
+              </Col>
+            </Row>
+            <hr />
+            <Row>
+              <Col sm="3">
+                <Card.Text>Role</Card.Text>
+              </Col>
+              <Col sm="9">
+                <Card.Text className="text-muted">
+                  {value.attributes.role}
+                </Card.Text>
+              </Col>
+            </Row>
+            <hr />
+            <Button onClick={() => handleClientClick(value)}>Open Calendar</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    ));
 
     // Render admin dashboard
     return(
@@ -104,6 +113,19 @@ function Clients (props){
                     </Row>
                 </Col>
             </Row>
+
+            <Modal show={show} onHide={() => setShow(false)}>
+            <Modal.Header closeButton>
+              <Modal.Title>Appointment Details</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <ClientCal user={props.user} client={selectedClient} />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="success">Save</Button>
+            </Modal.Footer>
+          </Modal>
+
         </Container>
     )
 }
